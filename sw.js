8772644f909c9ab082fa8e0bbf7c3c09cf621130
saadmfazal <saadfzl123@gmail.com>
2026-09-19 20:04:16 +0530

    Force fixed George PWA pairing build

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='morpheus-george-shell-v4';
+const CACHE='morpheus-george-shell-v5';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/morpheus.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
